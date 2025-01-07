14972b168ead3e57961c63e3cb85ae78d4d6de25
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2025-01-07 12:00:27 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
-import './css/main.css';
-
-import './js/fetchFilms.js';
-import './js/logWindow.js';
-import './js/mobileMenu.js';
-import './js/modal.js';
-import './js/modalButtons.js';
-import './js/pagination.js';
-import './js/popularFilms.js';
-import './js/modal-team.js';
-import './js/darkMode.js';
-import './js/to-top.js';
-import './js/buttonEffects.js';
-import './js/showFilms.js';
-import './js/searchFormListener.js';
-
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import './sass/main.scss';
+import './javascript/main.js';
